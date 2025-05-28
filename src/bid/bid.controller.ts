043c4d06ae4556d629cc873064d3b86914d4f46c
denis junior <denis_jr2001@hotmail.com>
2025-05-28 17:49:15 -0300

    feat: add JWT authentication guard to BidController

--- src/bid/bid.controller.ts
+++ src/bid/bid.controller.ts
@@ -6,11 +6,14 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { BidService } from './bid.service';
 import { CreateBidDto } from './dto/create-bid.dto';
 import { UpdateBidDto } from './dto/update-bid.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('bid')
 export class BidController {
   constructor(private readonly bidService: BidService) {}
