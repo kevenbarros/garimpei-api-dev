5ac44b8f0583317cc3db5cfec08fe49c9f40bdea
denis junior <denis_jr2001@hotmail.com>
2025-06-05 11:16:23 -0300

    feat: add console log for application startup with URL

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
 bootstrap();
