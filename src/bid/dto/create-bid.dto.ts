e832ff4462a0d8702597f11884e85d765aa71582
denis junior <denis_jr2001@hotmail.com>
2025-06-22 19:06:49 -0300

    feat: remove datetime field from CreateBidDto and clean up imports

--- src/bid/dto/create-bid.dto.ts
+++ src/bid/dto/create-bid.dto.ts
@@ -1,15 +1,11 @@
 import { Type } from 'class-transformer';
-import { IsNumber, IsString, ValidateNested } from 'class-validator';
+import { IsNumber, ValidateNested } from 'class-validator';
 import { CreateBuyerDto } from 'src/buyer/dto/create-buyer.dto';
 import { CreateClothingDto } from 'src/clothing/dto/create-clothing.dto';
 
 export class CreateBidDto {
   @IsNumber()
   bid: number;
-
-  @IsString()
-  datetime: string; // Ex: '2025-06-22T14:00:00'
-  // ...existing code...
 
   @ValidateNested({ each: true })
   @Type(() => CreateClothingDto)
